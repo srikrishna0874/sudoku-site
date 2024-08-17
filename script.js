0fd3adcd9cdd99d8f1e6b1543d18d82378d014cf
Kollipara Sri Krishna Venkata Subbarao <103669180+srikrishna0874@users.noreply.github.com>
2024-08-17 12:34:20 +0530

    added cell interactions

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const table = document.getElementById('sudoku-table');
 const tdElements = document.getElementsByTagName('td');
 
+console.log(tdElements.length);
+
 
 
 for (let i = 0; i < tdElements.length; i++) {
     tdElements[i].addEventListener('click', () => {
-        console.log(tdElements[i].cellIndex);
+        console.log(tdElements[i]);
+        for (let j = 0; j < tdElements.length; j++) {
+            tdElements[j].classList.remove('active-cell');
+        }
+        tdElements[i].classList.add('active-cell');
     });
 }
+
+
+// for (let i = 0; i < tdElements.length; i++) {
+//     tdElements[i].addEventListener('click', () => {
+//         tdElements[i].classList.toggle('active-cell');
+//     });
+// }
